58002ebef3f13f36f6887e14a8958896790a05e0
Sayan Nurmuhambet <s.deaiko@gmail.com>
2023-08-23 16:06:20 +0600

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const obj = {}
+    bracketsConfig.forEach(e => {
+        obj[e[0]] = e[1];
+    });
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        const e = str[i];
+        if (e in obj) {
+            if (obj[e] !== e || stack[stack.length - 1] !== e) {
+                stack.push(e); 
+            } else {
+                stack.pop();
+            }
+        } else {
+            key = stack[stack.length - 1];
+            if (e == obj[key]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 }
